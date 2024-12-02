87ed4ac11fb3fc4f4286a6c0a04d362c1e07ebb5
angemiJR <111733781+angemiJR@users.noreply.github.com>
2024-12-02 16:21:52 +0100

    adding components to main page

---
+++ src/components/Cards.jsx
@@ -0,0 +1,15 @@
+import "../components/Cards.css";
+function Cards() {
+    return (
+    <>
+        <div className="card">
+            
+           
+    
+        </div>
+    </>
+    )
+
+}
+
+export default Cards;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,14 +1,26 @@
 import "../components/Main.css";
+import Cards from "../components/Cards.jsx";
 
 function Main() {
     return (
-    <>
-        <div className="container">
-            
-           
-    
-        </div>
-    </>
+        <>
+            <div className="main">
+                <div className="header">
+
+                </div>
+                <div className="cards">
+                    <Cards></Cards>
+                    <Cards></Cards>
+                    <Cards></Cards>
+                    <Cards></Cards>
+                    <Cards></Cards>
+                    <Cards></Cards>
+                    <Cards></Cards>
+                    <Cards></Cards>
+                </div>
+
+            </div>
+        </>
     )
 
 }
