201a154c829f76c5c7e8e679d14bcd410cd0f3f5
angemiJR <111733781+angemiJR@users.noreply.github.com>
2024-12-02 16:45:40 +0100

    added some header, imported font

---
+++ src/components/Score.jsx
@@ -0,0 +1,17 @@
+
+
+import "../components/Main.css"
+function Score() {
+    return (
+    <>
+        <div>
+            
+           <h2>Your score is 125</h2>
+    
+        </div>
+    </>
+    )
+
+}
+
+export default Score;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,13 @@
 import "../components/Main.css";
 import Cards from "../components/Cards.jsx";
+import Score from "../components/Score.jsx";
 
 function Main() {
     return (
         <>
             <div className="main">
                 <div className="header">
-
+                    <Score></Score>
                 </div>
                 <div className="cards">
                     <Cards></Cards>
