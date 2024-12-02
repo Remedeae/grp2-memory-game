d935f825a03dbfa69e8947464fcab4f7c03fb423
angemiJR <111733781+angemiJR@users.noreply.github.com>
2024-12-02 14:21:10 +0100

    settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Main from "./pages/Main";
 
-
-function App() {}
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/main" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
 
 export default App;
